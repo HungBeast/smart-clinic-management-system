52273106496c4fe02dc7db87d72ec96f0d487673
HungBeast <hunghhse173110@fpt.edu.vn>
2025-10-07 16:01:37 +0700

    Create PatientRepository.java

---
+++ PatientRepository.java
@@ -0,0 +1,10 @@
+package com.yourorg.repository;
+
+import com.yourorg.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    // define custom queries if needed
+}
